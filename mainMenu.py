22b8a4d37905c3cc7b4d9171616832f59d861205
Mboesn <45824003+Mboesn@users.noreply.github.com>
2022-06-27 13:57:02 +0300

    reformatted main menu file

--- mainMenu.py
+++ mainMenu.py
@@ -5,23 +5,23 @@ from server import openNewWindow
 root = tki.Tk()
 root.geometry("300x300")
 T = 'CHAT'
-label = tki.Label(root, height=2, width=50,
-                  text='DO YOU WANT TO CHAT?')
-label.pack(pady=10)
+
+opening_msg_txt = tki.Label(root, height=2, width=50,
+                            text='DO YOU WANT TO EXCHANGE FILES?')
+opening_msg_txt.pack(pady=10)
+
+open_main_menu_btn = tki.Button(root,
+                                text="yes",
+                                command=lambda:
+                                clear_frame())
+open_main_menu_btn.pack()
 
 
 def clear_frame():
     for widgets in root.winfo_children():
         widgets.destroy()
 
-    PORTnIPin()
-
-
-printButton = tki.Button(root,
-                         text="yes",
-                         command=lambda:
-                         clear_frame())
-printButton.pack()
+    main_menu()
 
 
 def know_ip():
@@ -31,7 +31,7 @@ def know_ip():
     return ip1
 
 
-def putIN():
+def show_local_ip():
     ip1 = know_ip()
     Output3 = tki.Text(root, height=1,
                        width=30,
@@ -40,41 +40,48 @@ def putIN():
     Output3.pack(pady=5)
 
 
-def PORTnIPin():
-    Output = tki.Text(root, height=1,
+def main_menu():
+    ip_txt = tki.Text(root, height=1,
                       width=30,
                       bg="blue")
-    Output.insert(tki.END, 'PLEASE PUT YOUR IP INFO:')
-    Output.pack(pady=5)
-    IP5 = tki.Text(root, height=1, width=15)
-    IP5.pack(pady=5)
-    Output1 = tki.Text(root, height=1,
-                       width=30,
-                       bg="blue")
-    Output1.insert(tki.END, 'PLEASE PUT YOUR PORT INFO:')
-    Output1.pack(pady=5)
-    PORT1 = tki.Text(root, height=1, width=15)
-    PORT1.pack(pady=5)
-    Output2 = tki.Text(root, height=1,
-                       width=30,
-                       bg="blue")
-    Output2.insert(tki.END, 'DO YOU WANT TO KNOW YOUR IP?')
-    Output2.pack(pady=5)
-    printButton1 = tki.Button(root,
+    ip_txt.insert(tki.END, 'PLEASE PUT YOUR IP INFO:')
+    ip_txt.pack(pady=5)
+
+    ip = tki.Text(root, height=1, width=15)
+    ip.pack(pady=5)
+
+    port_txt = tki.Text(root, height=1,
+                        width=30,
+                        bg="blue")
+    port_txt.insert(tki.END, 'PLEASE PUT YOUR PORT INFO:')
+    port_txt.pack(pady=5)
+
+    port = tki.Text(root, height=1, width=15)
+    port.pack(pady=5)
+
+    local_ip_txt = tki.Text(root, height=1,
+                            width=30,
+                            bg="blue")
+    local_ip_txt.insert(tki.END, 'DO YOU WANT TO KNOW YOUR IP?')
+    local_ip_txt.pack(pady=5)
+
+    local_ip_btn = tki.Button(root,
                               text="yes",
                               command=lambda:
-                              putIN())
-    printButton1.pack()
-    Output4 = tki.Text(root, height=1,
-                       width=30,
-                       bg="red")
-    Output4.insert(tki.END, 'DONE?')
-    Output4.pack(pady=5)
-    printButton2 = tki.Button(root,
+                              show_local_ip())
+    local_ip_btn.pack()
+
+    confirmation_txt = tki.Text(root, height=1,
+                                width=30,
+                                bg="red")
+    confirmation_txt.insert(tki.END, 'DONE?')
+    confirmation_txt.pack(pady=5)
+
+    start_server = tki.Button(root,
                               text="yes",
                               command=lambda:
-                              openNewWindow(IP5, PORT1, root))
-    printButton2.pack()
+                              openNewWindow(ip, port, root))
+    start_server.pack()
 
 
 root.mainloop()
